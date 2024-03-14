992a1adda8398c83f38217a58f9314cb3ad71c63
0bou0 <alexis.canale@etu.univ-smb.fr>
2024-03-14 11:56:26 +0100

    ajout ProduitController + code methpde getAll

---
+++ FifApi/Controllers/ProduitsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FifApi.Models.EntityFramework;
+using Microsoft.EntityFrameworkCore.Internal;
+using NuGet.Protocol;
+
+namespace FifApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProduitsController : ControllerBase
+    {
+        private readonly FifaDBContext _context;
+
+        public ProduitsController(FifaDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Produits
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Produit>>> GetProduits()
+        {
+          if (_context.Produits == null)
+          {
+              return NotFound();
+          }
+            return _context.Produits
+                    .Join
+                    (
+                        inner: _context.CouleurProduits,
+                        outerKeySelector: p => p.Id,
+                        innerKeySelector: cp => cp.IdProduit,
+                        resultSelector: (p1, cp1) => new Produit
+                        { 
+                            Id = p1.Id,
+                            Name = p1.Name,
+                            Description = p1.Description,
+                            Caracteristiques = p1.Caracteristiques,
+                            CouleursProduit = _context.CouleurProduits.Where(cp => p1.Id == cp.IdProduit).ToList()
+                        }).ToList();
+               
+            
+        }
+
+        // GET: api/Produits/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Produit>> GetProduitById(int id)
+        {
+          if (_context.Produits == null)
+          {
+              return NotFound();
+          }
+            var produit = await _context.Produits.FindAsync(id);
+
+            if (produit == null)
+            {
+                return NotFound();
+            }
+
+            return produit;
+        }
+
+        // PUT: api/Produits/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProduit(int id, Produit produit)
+        {
+            if (id != produit.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(produit).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProduitExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Produits
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Produit>> PostProduit(Produit produit)
+        {
+          if (_context.Produits == null)
+          {
+              return Problem("Entity set 'FifaDBContext.Produits'  is null.");
+          }
+            _context.Produits.Add(produit);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProduit", new { id = produit.Id }, produit);
+        }
+
+        // DELETE: api/Produits/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduit(int id)
+        {
+            if (_context.Produits == null)
+            {
+                return NotFound();
+            }
+            var produit = await _context.Produits.FindAsync(id);
+            if (produit == null)
+            {
+                return NotFound();
+            }
+
+            _context.Produits.Remove(produit);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProduitExists(int id)
+        {
+            return (_context.Produits?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
