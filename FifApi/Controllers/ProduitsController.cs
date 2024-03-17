a3fd50892794f9b5cf3459913003c57aade379ef
0bou0 <alexis.canale@etu.univ-smb.fr>
2024-03-17 17:53:09 +0100

    ajout de GetProduitsByColor

--- FifApi/Controllers/ProduitsController.cs
+++ FifApi/Controllers/ProduitsController.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using FifApi.Models.EntityFramework;
 using Microsoft.EntityFrameworkCore.Internal;
 using NuGet.Protocol;
+using Microsoft.AspNetCore.Routing.Constraints;
 
 namespace FifApi.Controllers
 {
@@ -26,52 +27,70 @@ namespace FifApi.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<object>>> GetProduits()
         {
-          if (_context.Produits == null)
-          {
-              return NotFound();
-          }
+            if (_context.Produits == null)
+            {
+                return NotFound();
+            }
 
             return await (from p in _context.Produits
-                    join cp in _context.CouleurProduits on p.Id equals cp.IdProduit
-                    join a in _context.Albums on p.AlbumId equals a.IdAlbum into aGroup
-                    from a in aGroup.DefaultIfEmpty()
-                    join aph in _context.AlbumPhotos on a.IdAlbum equals aph.IdAlbum into aphGroup
-                    from aph in aphGroup.DefaultIfEmpty()
-                    join ph in _context.Photos on aph.IdPhoto equals ph.IdPhoto into phGroup
-                    from ph in phGroup.DefaultIfEmpty()
-                    group new { p, cp, a, aph, ph } by p.Id into g
-                    select new
-                    {
-                        idProduct = g.Key,
-                        title = g.FirstOrDefault()!.p.Name,
-                        price = g.Min(x => x.cp.Prix),
-                        image = g.FirstOrDefault()!.ph.URL
-                    }).ToListAsync();
+                join cp in _context.CouleurProduits on p.Id equals cp.IdProduit
+                join a in _context.Albums on p.AlbumId equals a.IdAlbum into aGroup
+                from a in aGroup.DefaultIfEmpty()
+                join aph in _context.AlbumPhotos on a.IdAlbum equals aph.IdAlbum into aphGroup
+                from aph in aphGroup.DefaultIfEmpty()
+                join ph in _context.Photos on aph.IdPhoto equals ph.IdPhoto into phGroup
+                from ph in phGroup.DefaultIfEmpty()
+                group new { p, cp, a, aph, ph } by p.Id into g
+                select new
+                {
+                    idProduct = g.Key,
+                    title = g.FirstOrDefault()!.p.Name,
+                    price = g.Min(x => x.cp.Prix),
+                    image = g.FirstOrDefault()!.ph.URL,
+                    couleurs = (from cp in _context.CouleurProduits
+                    join c in _context.Couleurs on cp.IdCouleur equals c.Id
+                    where cp.IdProduit == g.Key
+                    select new {
+                        prix = cp.Prix,
+                        codebarre = cp.CodeBarre,
+                        couleur = c.Nom
+                    }).ToList()
+                }).ToListAsync();
         }
 
         // GET: api/Produits/5
         [HttpGet("{id}")]
         public async Task<ActionResult<object>> GetProduitById(int id)
         {
-          if (_context.Produits == null)
-          {
-              return NotFound();
-          }
+            if (_context.Produits == null)
+            {
+                return NotFound();
+            }
+
             var produit = await (from p in _context.Produits
-                    join a in _context.Albums on p.AlbumId equals a.IdAlbum into aGroup
-                    from a in aGroup.DefaultIfEmpty()
-                    join aph in _context.AlbumPhotos on a.IdAlbum equals aph.IdAlbum into aphGroup
-                    from aph in aphGroup.DefaultIfEmpty()
-                    join ph in _context.Photos on aph.IdPhoto equals ph.IdPhoto into phGroup
-                    from ph in phGroup.DefaultIfEmpty()
-                    group new { p, a, aph, ph } by p.Id into g
-                    select new
-                    {
-                        idProduct = g.Key,
-                        title = g.FirstOrDefault()!.p.Name,
-                        image = g.FirstOrDefault()!.ph.URL,
-                        couleurs = _context.CouleurProduits.Where(x => x.IdProduit == g.Key).ToList()
-                    }).Where(x => x.idProduct == id).ToListAsync();
+                join a in _context.Albums on p.AlbumId equals a.IdAlbum into aGroup
+                from a in aGroup.DefaultIfEmpty()
+                join aph in _context.AlbumPhotos on a.IdAlbum equals aph.IdAlbum into aphGroup
+                from aph in aphGroup.DefaultIfEmpty()
+                join ph in _context.Photos on aph.IdPhoto equals ph.IdPhoto into phGroup
+                from ph in phGroup.DefaultIfEmpty()
+                group new { p, a, aph, ph } by p.Id into g
+                select new
+                {
+                    idProduct = g.Key,
+                    title = g.FirstOrDefault()!.p.Name,
+                    description = g.FirstOrDefault()!.p.Description,
+                    caracteristiques = g.FirstOrDefault()!.p.Caracteristiques,
+                    image = g.FirstOrDefault()!.ph.URL,
+                    couleurs = (from cp in _context.CouleurProduits
+                    join c in _context.Couleurs on cp.IdCouleur equals c.Id
+                    where cp.IdProduit == g.Key
+                    select new {
+                        prix = cp.Prix,
+                        codebarre = cp.CodeBarre,
+                        couleur = c.Nom
+                    }).ToList()
+                }).Where(x => x.idProduct == id).ToListAsync();
 
             if (produit == null)    
             {
@@ -79,6 +98,50 @@ namespace FifApi.Controllers
             }
 
             return produit;
+        }
+
+        [HttpGet("Couleur/{couleur}")]
+        public async Task<ActionResult<object>> GetProduitsByColor(string couleur)
+        {
+            if (_context.Produits == null)
+            {
+                return NotFound();
+            }
+
+            return await (from p in _context.Produits
+                    where _context.Couleurs
+                        .Where(c => c.Nom == couleur)
+                        .SelectMany(c => _context.CouleurProduits
+                            .Where(cp => cp.IdCouleur == c.Id && cp.IdProduit == p.Id))
+                        .Any()
+                    select new
+                    {
+                        idProduct = p.Id,
+                        title = p.Name,
+                        price = _context.CouleurProduits
+                            .Where(cp => cp.IdProduit == p.Id)
+                            .Min(cp => cp.Prix),
+                        image = _context.Photos
+                            .Join
+                            (_context.AlbumPhotos, alb => alb.IdPhoto, pth => pth.IdAlbum, (pth, alb) => new { photo = pth.URL })
+                            .Select(aph => aph.photo)
+                            .FirstOrDefault(),
+                        couleurs = _context.Couleurs
+                            .Select(c => new
+                            {
+                                prix = _context.CouleurProduits
+                                    .Where(cp => cp.IdProduit == p.Id && cp.IdCouleur == c.Id)
+                                    .Select(cp => cp.Prix)
+                                    .FirstOrDefault(),
+                                codebarre = _context.CouleurProduits
+                                    .Where(cp => cp.IdProduit == p.Id && cp.IdCouleur == c.Id)
+                                    .Select(cp => cp.CodeBarre)
+                                    .FirstOrDefault(),
+                                couleur = c.Nom
+                            }).ToList()
+                    }).ToListAsync();
+
+
         }
 
         // PUT: api/Produits/5
