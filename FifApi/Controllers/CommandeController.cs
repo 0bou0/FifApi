674d8adac49efc51dcd9cf83dc92257f635fca90
Romain Weigel <Romain.Weigel@etu.univ-savoie.fr>
2024-03-19 06:23:51 +0100

    Création du controller commande

---
+++ FifApi/Controllers/CommandeController.cs
@@ -0,0 +1,61 @@
+﻿using FifApi.Models.EntityFramework;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FifApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandeController : ControllerBase
+    {
+        private readonly FifaDBContext _context;
+
+        public CommandeController(FifaDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Produits
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<object>>> GetCommandes()
+        {
+            if (_context.Produits == null)
+            {
+                return NotFound();
+            }
+
+
+
+            return await(from co in _context.Commandes
+                         join ut in _context.Utilisateurs on co.IdUtilisateur equals ut.IdUtilisateur
+                         join lc in _context.LigneCommandes on co.IdCommande equals lc.IdCommande
+                         join st in _context.Stocks on lc.IdStock equals st.IdStock
+                         group new { co, ut, lc, st } by co.IdCommande into g
+                         select new
+                         {
+                            IdCommande = g.Key,
+                            Pseudo = g.FirstOrDefault()!.ut.PseudoUtilisateur,
+                            NomUtilisateur = g.FirstOrDefault()!.ut.NomUtilisateur,
+                            PrenomUtilisateur = g.FirstOrDefault()!.ut.PrenomUtilisateur,
+                            LigneCommandes = (
+                                from lc in _context.LigneCommandes
+                                join st in _context.Stocks on lc.IdStock equals st.IdStock
+                                where lc.IdCommande == g.Key
+                                    select new
+                                    {
+                                        LigneCommande = lc.CommandeLigne,
+                                        Stock = st.IdStock,
+                                        Quantite = st.Quantite
+                                    }
+                            ).ToList()
+
+
+                         }
+
+                ).ToListAsync();
+
+          
+        }
+    }
+}
