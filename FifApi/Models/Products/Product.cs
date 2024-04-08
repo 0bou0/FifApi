9e6e73705692c1d266deffa91d631edfb644005c
0bou0 <alexis.canale@etu.univ-smb.fr>
2024-04-08 13:40:03 +0200

    PostProduit fonctionnel, mais un peut goofy

---
+++ FifApi/Models/Products/Product.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Runtime.CompilerServices;
+
+namespace FifApi.Models.Products
+{
+    public class Product
+    {
+        public string NomProduit { get; set; }
+        public string DescriptionProduit {  get; set; }
+        public string Image { get; set; }
+        public int Marque { get; set; }
+        public string Nation { get; set; }
+        public int CategorieId { get; set; }
+        public List<Color>? Couleurs { get; set; }
+
+    }
+}
